467bd910f98e43090cd7a424b87b2ec115f25ebc
renanmarquespereira <renanmarques31@gmail.com>
2025-10-04 23:26:39 -0300

    Quinto Commit

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,13 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-
 from fakepinterest.models import Usuario
 
 
 class FormLogin(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20, message='A senha precisa ter no mínimo 8 carateres!')])
     submit = SubmitField('Enviar')
     botaoLogin = SubmitField('Login')
 
@@ -16,30 +15,21 @@ class FormLogin(FlaskForm):
         if not usuario:
             raise ValidationError("Email não existe!")
 
-    def validate_senha(self, senha):
-        if len(senha.data) < 8:
-            raise ValidationError("Senha deve ter mais pelo menos 8 caracteres")
+    def fazerLogin(self):
+        email = self.email.data
+        senha = self.senha.data
 
 class FormCriarConta(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     username = StringField('Username', validators=[DataRequired()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20)])
-    confirmarSenha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha')])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(8, 20, message='A senha precisa ter no mínimo 8 carateres!')])
+    confirmarSenha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha', message='As senhas não coincidem.')])
     botaoCriarConta = SubmitField('Criar Conta')
 
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
             raise ValidationError("Email ja cadastrado!")
-
-    def validate_senha(self, senha):
-        if len(senha.data) < 8:
-            raise ValidationError("Senha deve ter mais pelo menos 8 caracteres")
-
-
-    def validate_confirmarSenha(self, confirmarSenha):
-        if self.senha != confirmarSenha.data:
-            raise ValidationError("Senha invalida, tamanho tem que ser 8/20")
 
 class FormFoto(FlaskForm):
     foto = FileField('Foto', validators=[DataRequired()])

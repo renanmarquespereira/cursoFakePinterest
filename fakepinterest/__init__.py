b9ca82d5ba8e588ba519973a351ee7d9ea2d463e
renanmarquespereira <163781666+renanmarquespereira@users.noreply.github.com>
2025-10-05 12:16:17 -0300

    Update __init__.py

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -6,7 +8,14 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 #Criando Banco de dados
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fakebase.db"
+
+
+if os.getenv("DEBUG") == 0:
+    link_bd = os.getenv("DATABASE_URL")
+else:
+    link_bd = "sqlite:///fakebase.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = link_bd
 # CHAVE DE SEGURANÇA DO APP
 app.config["SECRET_KEY"] = "bgbP_CsFvALHnd29V3CW3A"
 #Diz onde quero salvar imagens no meu programa

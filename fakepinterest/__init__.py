e12ad5bef5057a4cc3a5d588641bb7a9baa8be3a
renanmarquespereira <renanmarques31@gmail.com>
2025-10-22 06:54:10 -0300

    adicionado PRocFile

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 #Criando Banco de dados
 
 
-if os.getenv("DEBUG") == "0":
-    link_bd = os.getenv("DATABASE_URL")
+if os.getenv("DATABASE_URL"):
+    link_bd = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 else:
     link_bd = "sqlite:///fakebase.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = link_bd
+
 # CHAVE DE SEGURANÇA DO APP
 app.config["SECRET_KEY"] = "bgbP_CsFvALHnd29V3CW3A"
 #Diz onde quero salvar imagens no meu programa
